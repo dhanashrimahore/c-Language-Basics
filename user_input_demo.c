3b0ae4d9a19784082194a0e5fd3481e4dcbc767a
dhanashrimahore <dhanashri3011@gmail.com>
2026-08-23 12:17:05 +0530

    Create user_input_demo.c

---
+++ user_input_demo.c
@@ -0,0 +1,13 @@
+// Program to take user input (Age) using scanf
+#include <stdio.h>
+
+int main() {
+    int age;
+
+    printf("Enter your age: ");
+    scanf("%d", &age)
+
+    printf("Your age is: %d\n, age);
+
+    return 0;
+}
