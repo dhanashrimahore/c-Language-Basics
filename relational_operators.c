0644ee03e2e3039762d624cdcb28b38030112024
dhanashrimahore <dhanashri3011@gmail.com>
2026-08-23 19:12:45 +0530

    Create relational_operators.c

---
+++ relational_operators.c
@@ -0,0 +1,13 @@
+// Program to demonstrate Relational Operators in C
+#include <stdio.h>
+int main()
+{
+    int a = 10;
+    int b = 20;
+
+    printf("%d\n", a > b);   // Returns 0 (False)
+    printf("%d\n", a < b);   // Returns 1 (True)
+    printf("%d\n", a == b);  // Returns 0 (False)
+
+    return 0;
+}
