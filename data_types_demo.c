bc1f023be459d440ab5f4879e842429a787336d4
dhanashrimahore <dhanashri3011@gmail.com>
2026-08-23 11:56:41 +0530

    Create data_types_demo.c

---
+++ data_types_demo.c
@@ -0,0 +1,15 @@
+// Program to demonstrate C Data Types (int, float, char)
+#include <stdio.h>
+
+int main() 
+{
+    int age = 18;
+    float marks = 19.20;
+    char grade = A;
+
+    printf("%d\n", age);
+    printf("%f\n", marks);
+    printf("%c\n", grade);
+
+    return 0;
+}
